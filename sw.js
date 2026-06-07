182c980430ff31c9dbbc79b34753e487f6d625cf
w5mpz965t8-rgb <w5mpz965t8@privaterelay.appleid.com>
2026-06-07 18:16:54 +0300

    Rename sw.js.txt to sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'kelime-dunyasi-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : Promise.resolve()))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then(response => {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
+});
